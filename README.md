e9c0ff5cc799be168ac5a587dd1539272590b3e8
JeongHojin96 <JeongHojin96@JeongHojin96.com>
2023-06-19 15:43:41 +0900

    iolist

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# Java_IoList Project
+# Java IoList Project
 - java, Oracle를 사용한 Console Project
-- eClipse 의 Console 환경에서 Java 를 사용한 DBMS 핸들링 
+- eClipse 의 Console 환경에서 java 를 사용한 DBMS 핸들링
 - 상품관리, 고객관리, 상품구매(장바구니)를 관리하는 Project
 
 ## 필요한 3rd party LIB
 - ojdbc*.jar, mybatis*.jar
+
+## Maven Project
+- java project 로 설정된 기본 형식을 Maven Project 로 변환한다 : 프로젝트에서 우클릭 > configure > convert maven project
+- pom.xml 이라는 파일이 생성된다
+
+### POM.xml
+- POM : `Project Object Model` 프로젝트에 필요한 3rd JDK를 쉽게 관리하기 위한 도구
+- Maven Project 를 컴파일하고, Build 하는데 필요한 설정 정보를 담고있는 파일
---
+++ document/장바구니(2023-06-19).sql
@@ -0,0 +1,91 @@
+-- iolist User 화면
+
+-- 현재 설치된 Oracle DBMS 에 포함된 모든 Table 을 보여달라
+SELECT * FROM all_tables;
+-- iolist User 가 접근(CRUD 실행)할수 있는 Table을 보여달라
+SELECT * FROM all_tables WHERE OWNER = 'IOLIST';
+
+-- tbl_product TABLE 의 속성(Domain)을 보여달라
+-- DBMS 에서 Domain : table 의 속성(칼럼)의 데이터 type 
+DESCRIBE tbl_product;
+
+INSERT INTO tbl_buyer ( buid, buname, butel, buaddr)
+VALUES('0000000001', '홍길동', '010-1111-1111', '서울특별시');
+
+INSERT INTO tbl_buyer ( buid, buname, butel, buaddr)
+VALUES('0000000002', '성춘향', '010-2222-2222', '전라북도 남원시');
+
+INSERT INTO tbl_buyer ( buid, buname, butel, buaddr)
+VALUES('0000000003', '이몽룡', '010-3333-3333', '한양시특별시');
+
+INSERT INTO tbl_buyer ( buid, buname, butel, buaddr)
+VALUES('0000000004', '김길동', '010-4444-4444', '함경도 함응');
+
+INSERT INTO tbl_buyer ( buid, buname, butel, buaddr)
+VALUES('0000000005', '이길용', '010-5555-5555', '광주광역시');
+
+COMMIT;
+SELECT * FROM tbl_buyer;
+SELECT * FROM tbl_buyer WHERE buid = '0000000001';
+SELECT * FROM tbl_buyer WHERE buid = '0000000007';
+
+-- PK 이외의 칼럼을 기준으로 조회하면 비록 1개의 결과만 보일지라도 
+-- 이 결과는 1개 이상인 것으로 본다.
+-- 이 결과는 empty 이거나 1개 이상이다.
+SELECT * FROM tbl_buyer WHERE buName = '홍길동';
+/*
+중간 문자열 검색
+Full Text Search : 칼럼의 데이터 문자열을 모두 검색하여 일치하는 데이터 찾기
+SELECT 조회 명령 실행 과정에서 가장 느린 Query
+
+칼럼의 데이터 중에 일부라도 일치하는 문자열이 있는 데이터 조회하기
+*/
+
+-- 이름 중간 문자열 검색
+SELECT * FROM tbl_buyer WHERE buName LIKE '홍%';
+SELECT * FROM tbl_buyer WHERE buName LIKE '%동%';
+
+-- 연결된 문자열 생성하기
+SELECT '강' || '가' || '딘' FROM dual;
+
+/*
+표준 SQL 에서는 아주 단순한 연산을 수행하는 SELECT 문을 지원한다.
+숫자의 사칙연산, 문자열 연결하기 등 간단히 SELECT 명령을 통하여
+결과를 확인할 수 있다.
+
+그런데 ORACLE 에서는 FROM(절) 키워드가 생략된 SELECT 명령을 활용하지 않는다.
+그래서 의미없는 DUMMY Table 을 하나 마련해 두었다.
+Oracle 에서 단순 연산을 위한 SELECT 에서는 dual table 을 사용하여 수행한다.
+*/
+SELECT 30 + 40; -- 표준 SQL 문법
+SELECT * FROM dual;
+SELECT 30 + 40 FROM dual;
+
+SELECT 30 + 40 FROM tbl_buyer;
+
+-- 문자열 연결 이때 '||' 을 사용하여 연결
+SELECT '강' || '가' || '딘' FROM dual;
+
+-- 사칙연산
+SELECT 30 + 40 FROM dual;
+SELECT 30 - 40 FROM dual;
+SELECT 30 * 40 FROM dual;
+SELECT 30 / 40 FROM dual;
+
+-- 연산
+SELECT 30 + 40 + 50 + 60 FROM dual;
+
+-- 함수를 사용한 연산
+-- Table에 데이터가 몇개 있느냐?
+SELECT COUNT(*) FROM tbl_buyer;
+
+-- 이름에 길 문자열이 포함된 고객이 몇명이냐
+SELECT COUNT(*) FROM tbl_buyer WHERE buName LIKE '%길%' ;
+
+-- 현재 저장된 데이터 주엥서 가장 큰 buid 값은 얼마냐
+SELECT MAX(buid) FROM tbl_buyer;
+
+-- 현재 저장된 데이터 중에서 가장 작은 buid 값은 얼마냐
+SELECT MIN(buid) FROM tbl_buyer;
+
+
